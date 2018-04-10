12142fe04ef90c3acaf26b7be7536a49f10406be
mmdets <37974802+mmdets@users.noreply.github.com>
2018-04-10 13:14:14 +0300

    H4

--- 20.1.py
+++ 20.1.py
@@ -1,17 +1,17 @@
 import sys
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.lang.builder import Builder
-from kivy.uix.widget import Widget
 from kivy.properties import StringProperty, ObjectProperty
 
 def cel(fahr):
     return (5 / 9 * (fahr - 32)) 
 
-class Convert(BoxLayout):
+class Convert(FloatLayout):
 
     inputElement = ObjectProperty(None)
     content = StringProperty(None)
@@ -21,9 +21,12 @@ class Convert(BoxLayout):
     def __init__(self, **kwargs):
         super(Convert, self).__init__(**kwargs)
 
-    def convert_press(self, fahr):
+    def output(self, fahr):
         self.content = fahr
-        self.out = str(round(cel(int(self.content)), 4))
+        self.out = str(round(cel(int(self.content)), 2))
+
+    def convert_press(self,fahr):
+        self.output(fahr)
 
     def quit(self):
         sys.exit()
